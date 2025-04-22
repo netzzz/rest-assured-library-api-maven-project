c97400028d9afa028454aa019ec3a7c8ae86f60d
nemanja <nemanja.stevanovic911@gmail.com>
2025-04-22 10:15:49 +0200

    Remove unused code

--- src/test/java/data/LibraryApiData.java
+++ src/test/java/data/LibraryApiData.java
@@ -1,31 +1,10 @@
 package data;
 
 public class LibraryApiData {
-	public static String addBookRequestBody(String bookName, String bookIsbn, String bookAisle, String bookAuthor) {
-		return "{\r\n"
-				+ "\r\n"
-				+ "\"name\":\""+bookName+"\",\r\n"
-				+ "\"isbn\":\""+bookIsbn+"\",\r\n"
-				+ "\"aisle\":\""+bookAisle+"\",\r\n"
-				+ "\"author\":\""+bookAuthor+"\"\r\n"
-				+ "}\r\n"
-				+ "";
-	}
-	
-	public static String deleteBookRequestBody(String bookId) {
-		return "{\r\n"
-				+ " \r\n"
-				+ "\"ID\" : \""+bookId+"\"\r\n"
-				+ " \r\n"
-				+ "} \r\n"
-				+ "";
-	}
-	
 	public static enum LibraryApiResponseMessages {
 	    SUCCESSFULLYADDED("successfully added"),
 	    SUCCESSFULLYDELETED("book is successfully deleted");
 		
-
 	    private final String message;
 
 	    // Constructor
